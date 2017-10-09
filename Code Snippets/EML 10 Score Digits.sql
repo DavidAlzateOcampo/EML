8aa375aa705b96b71cce66bbc4a044b50d09e934
SAP HANA Academy <hanaacademy@sap.com>
2017-10-09 14:21:43 +0200

    SAP HANA External Machine Learning Library

--- Code Snippets/EML 10 Score Digits.sql
+++ Code Snippets/EML 10 Score Digits.sql
@@ -13,7 +13,9 @@ CALL "SYS"."AFLLANG_WRAPPER_PROCEDURE_DROP" ('EMLUSER', 'SCORE_DIGITS');
 -- create table types
 CREATE TYPE "T_PARAMS" AS TABLE ("Parameter" VARCHAR(100), "Value" VARCHAR(100));
 CREATE TYPE "T_DATA" AS TABLE ("x" BLOB);
+-- use with rev 20
 CREATE TYPE "T_RESULTS" AS TABLE ("Classes" VARCHAR(100), "Scores" FLOAT);
+-- use with rev 21
 --CREATE TYPE "T_RESULTS" AS TABLE ("Class1" VARCHAR(100), "Class2" VARCHAR(100), "Class3" VARCHAR(100), "Class4" VARCHAR(100), "Class5" VARCHAR(100), "Class6" VARCHAR(100), "Class7" VARCHAR(100), "Class8" VARCHAR(100), "Class9" VARCHAR(100), "Class10" VARCHAR(100), "Score1" FLOAT, "Score2" FLOAT, "Score3" FLOAT, "Score4" FLOAT, "Score5" FLOAT, "Score6" FLOAT, "Score7" FLOAT, "Score8" FLOAT, "Score9" FLOAT, "Score10" FLOAT);
 
 -- create signature table then generate stored procedure
